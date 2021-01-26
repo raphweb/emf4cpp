0cfc62882f5975d78208811d0256eeeff68f163e
Raphael Weber <raphael.weber@syne.de>
2021-01-26 19:45:55 +0100

    Trying out project name replacement in templates.

--- emf4cpp/bootstrap.sh
+++ emf4cpp/bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+mv ../org.csu.emf4cpp.generator/src/metamodel/Ecore.ecore ../org.csu.emf4cpp.generator/src/metamodel/emf4cpp.ecore
+
 ../org.csu.emf4cpp.generator/emf4cpp.generator.sh \
     --internal --bootstrap \
-    ../org.csu.emf4cpp.generator/src/metamodel/Ecore.ecore
+    ../org.csu.emf4cpp.generator/src/metamodel/emf4cpp.ecore
+
+mv ../org.csu.emf4cpp.generator/src/metamodel/emf4cpp.ecore ../org.csu.emf4cpp.generator/src/metamodel/Ecore.ecore
